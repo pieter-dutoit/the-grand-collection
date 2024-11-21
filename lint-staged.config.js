fdd0798a6aa216debaf2bb5e84fa7d73156a666d
Pieter Du Toit <pdut8901@gmail.com>
2024-11-21 14:09:44 +1000

    chore: update staged linter

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 
+const formatCommand = "prettier --write .";
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand]
+  "*.{js,jsx,ts,tsx}": [formatCommand, buildEslintCommand]
 };
