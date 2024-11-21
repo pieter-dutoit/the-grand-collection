11efb0eec759bc896ddde214e672d4d38dbd0159
Pieter Du Toit <pdut8901@gmail.com>
2024-11-21 14:00:05 +1000

    chore: update lint config

--- src/ui/footer/index.tsx
+++ src/ui/footer/index.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className='fixed bottom-0 flex w-full justify-center bg-white'>
-      <div className='w-full max-w-[1024px] px-6'>Footer content</div>
+      <div className='w-full max-w-lg px-6'>Footer content</div>
     </footer>
   );
 }
