851f13dfad7376c3aca2201d862db31d7031f13b
Pieter Du Toit <pdut8901@gmail.com>
2024-11-21 14:01:13 +1000

    chore: update lint config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import { Footer } from "@/ui/footer";
-import { Navbar } from "@/ui/navbar";
-import { ThemeToggle } from "@/ui/theme-toggle";
-
 export default function Home() {
   return (
     <>
       <main style={{ height: "200vh" }}>
-        <h1>Main page</h1>
+        <h1>Main page 2</h1>
       </main>
     </>
   );
