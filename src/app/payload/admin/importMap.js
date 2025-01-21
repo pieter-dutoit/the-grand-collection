fcfc47d7ed2d62bd2227be687853583ddf21881a
Pieter du Toit <pdut8901@gmail.com>
2025-01-21 13:10:16 +1000

    chore: add cache revalidation function to cms dashboard

--- src/app/payload/admin/importMap.js
+++
@@ -1,5 +0,0 @@
-import { default as default_7a56bc7053abd82915e399b29232abcd } from '@/payload/components/file-size-display'
-
-export const importMap = {
-  "@/payload/components/file-size-display#default": default_7a56bc7053abd82915e399b29232abcd
-}
