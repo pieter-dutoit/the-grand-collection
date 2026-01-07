66f5690f0494d0058620b1e0500ac572d78c6d07
Pieter du Toit <pdut8901@gmail.com>
2026-01-08 09:32:33 +1100

    chore: cleanup

--- src/components/rich-text.tsx
+++ src/components/rich-text.tsx
@@ -172,7 +172,7 @@ export function ArticleRichText({ data, className, headings }: Props) {
       data={data}
       converters={createJsxConverters(headingIds)}
       className={twMerge(
-        'prose max-w-none prose-headings:font-light prose-p:leading-relaxed prose-p:text-olive-800 prose-blockquote:text-olive-800 prose-figcaption:text-xs prose-figcaption:font-semibold prose-figcaption:text-olive-500 prose-strong:text-olive-900 prose-li:opacity-90 prose-img:rounded-2xl',
+        'prose max-w-none prose-headings:font-light prose-h2:font-semibold prose-p:leading-relaxed prose-p:text-olive-800 prose-blockquote:text-olive-800 prose-figcaption:text-xs prose-figcaption:font-semibold prose-figcaption:text-olive-500 prose-strong:text-olive-900 prose-li:opacity-90 prose-img:rounded-2xl',
         className
       )}
     />
